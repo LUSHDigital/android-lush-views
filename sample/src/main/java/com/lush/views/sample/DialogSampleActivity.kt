25e1e9fb0ed854c08ff5aba75a896d5a93ec556d
Matt Allen <matthew.allen@lush.co.uk>
2018-01-19 15:48:46 +0000

    Added the status dialog examples to the dialogs activity

--- sample/src/main/java/com/lush/views/sample/DialogSampleActivity.kt
+++ sample/src/main/java/com/lush/views/sample/DialogSampleActivity.kt
@@ -2,6 +2,8 @@ package com.lush.views.sample
 
 import android.os.Bundle
 import com.lush.views.dialogs.ExampleMessageDialog
+import com.lush.views.dialogs.StatusFailureDialog
+import com.lush.views.dialogs.StatusSuccessDialog
 import com.lush.views.sample.base.BaseSampleActivity
 import kotlinx.android.synthetic.main.activity_dialogs.*
 
@@ -11,6 +13,8 @@ class DialogSampleActivity: BaseSampleActivity()
 	{
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_dialogs)
-		message.setOnClickListener { ExampleMessageDialog().show(supportFragmentManager) }
+		message.setOnClickListener { ExampleMessageDialog().show(fragmentManager) }
+		status_success.setOnClickListener { StatusSuccessDialog().show(fragmentManager) }
+		status_failure.setOnClickListener { StatusFailureDialog().show(fragmentManager) }
 	}
 }
