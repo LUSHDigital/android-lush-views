f15e96ba4d6609df79176cad2a97728b1437f11f
Jamie Cruwys <me@jamiecruwys.co.uk>
2018-03-21 13:29:38 +0000

    Refactored adapters, view holders and on click listeners into Kotlin and ensured that there cannot be any null models in the list

---
+++ views/src/main/java/com/lush/lib/adapter/BaseSelectableListAdapter.kt
@@ -0,0 +1,55 @@
+package com.lush.lib.adapter
+
+import com.lush.lib.listener.OnListItemClickListener
+import com.lush.view.holder.BaseSelectableViewHolder
+import com.lush.view.holder.BaseViewHolder
+import java.util.*
+
+/**
+ * An extension over the [BaseListAdapter] which allows for items in the list to be
+ * marked as selected. This is particularly useful in a situation for selecting multiple things.
+ */
+abstract class BaseSelectableListAdapter<T>(items: ArrayList<T> = arrayListOf(), listener: OnListItemClickListener<T>? = null) : BaseListAdapter<T>(items, listener)
+{
+	private var selected: ArrayList<T> = arrayListOf()
+
+	override fun onBindViewHolder(holder: BaseViewHolder<T>, position: Int)
+	{
+		super.onBindViewHolder(holder, position)
+
+		if (holder is BaseSelectableViewHolder<*>)
+		{
+			val item = getItem(position)
+			(holder as BaseSelectableViewHolder<*>).setSelected(getSelected().contains(item))
+		}
+	}
+
+	fun getSelected(): ArrayList<T>
+	{
+		return selected
+	}
+
+	fun addSelected(item: T)
+	{
+		selected.add(item)
+		val position = getPosition(item)
+		notifyItemChanged(position)
+	}
+
+	fun removeSelected(item: T)
+	{
+		selected.remove(item)
+		val position = getPosition(item)
+		notifyItemChanged(position)
+	}
+
+	fun setSelected(item: T)
+	{
+		val selectedCopy = ArrayList(selected)
+		for (toRemove in selectedCopy)
+		{
+			removeSelected(toRemove)
+		}
+		addSelected(item)
+	}
+}
---
+++ views/src/main/java/com/lush/view/holder/BaseViewHolder.kt
@@ -0,0 +1,23 @@
+package com.lush.view.holder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+/**
+ * Base class for all view holders to be used in concrete implementations of [com.lush.lib.adapter.BaseListAdapter]
+ */
+abstract class BaseViewHolder<in T>(view: View) : RecyclerView.ViewHolder(view)
+{
+	abstract fun bind(model: T)
+
+	fun setOnClickListener(listener: (View) -> Unit)
+	{
+		itemView.setOnClickListener(listener)
+	}
+
+	/**
+	 * Override this method to cancel any behaviour you don't want to continue after the view has been recycled
+	 * This is most likely async behaviour
+	 */
+	fun recycle() { }
+}
