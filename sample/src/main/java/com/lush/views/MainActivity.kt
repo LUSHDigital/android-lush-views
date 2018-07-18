58bafcf6dcc77157c8ec6446b987c448dbb66aaa
Matt Allen <matthew.allen@lush.co.uk>
2018-07-18 14:42:45 +0100

    Created a piece in the sample to show the alternate colouring of lists

--- sample/src/main/java/com/lush/views/MainActivity.kt
+++ sample/src/main/java/com/lush/views/MainActivity.kt
@@ -6,23 +6,26 @@ import com.lush.views.sample.*
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
-class MainActivity : BaseActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity()
+{
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
 
-        javalist.setOnClickListener { startActivity<JavaListSampleActivity>() }
-        list.setOnClickListener { startActivity<ListSampleActivity>() }
-        buttons.setOnClickListener { startActivity<ButtonsSampleActivity>() }
-        text.setOnClickListener { startActivity<TextSampleActivity>() }
-        quantity.setOnClickListener { startActivity<QuantitySampleActivity>() }
-        spinners.setOnClickListener { startActivity<SpinnerSampleActivity>() }
-        ratings.setOnClickListener { startActivity<RatingSampleActivity>() }
-        forms.setOnClickListener { startActivity<FormSampleActivity>() }
-        youtube.setOnClickListener { startActivity<YoutubeSampleActivity>() }
-        webviews.setOnClickListener { startActivity<WebViewSampleActivity>() }
-        images.setOnClickListener { startActivity<ImageSampleActivity>() }
-        wizard.setOnClickListener { startActivity<WizardSampleActivity>() }
-        dialogs.setOnClickListener { startActivity<DialogSampleActivity>()}
-    }
+		javalist.setOnClickListener { startActivity<JavaListSampleActivity>() }
+		list.setOnClickListener { startActivity<ListSampleActivity>() }
+		buttons.setOnClickListener { startActivity<ButtonsSampleActivity>() }
+		text.setOnClickListener { startActivity<TextSampleActivity>() }
+		quantity.setOnClickListener { startActivity<QuantitySampleActivity>() }
+		spinners.setOnClickListener { startActivity<SpinnerSampleActivity>() }
+		ratings.setOnClickListener { startActivity<RatingSampleActivity>() }
+		forms.setOnClickListener { startActivity<FormSampleActivity>() }
+		youtube.setOnClickListener { startActivity<YoutubeSampleActivity>() }
+		webviews.setOnClickListener { startActivity<WebViewSampleActivity>() }
+		images.setOnClickListener { startActivity<ImageSampleActivity>() }
+		wizard.setOnClickListener { startActivity<WizardSampleActivity>() }
+		dialogs.setOnClickListener { startActivity<DialogSampleActivity>() }
+		coloured_list.setOnClickListener { startActivity<ColouredListActivity>() }
+	}
 }
